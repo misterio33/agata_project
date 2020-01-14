97215e0f88afd9b37ee49c169bbc8f4e8f15ab7b
misterio33 <pasha.onoprienko@gmail.com>
2020-01-14 13:38:56 +0200

    requirements.txt for virtual environment + skeleton

---
+++ agata_programm_folder/agata_skeleton.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib as plt
+import scipy
+import mahotas
+import SimpleITK
+import sklearn
+
+
+def numpyarray(value):
+    value = np.array([value]) #numpy for cheking this library via virtual environment
+    print(value)
+
+
+numpyarray(14)
