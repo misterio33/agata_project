8fff48c916a427798af32cda87158319031da69f
misterio33 <pasha.onoprienko@gmail.com>
2020-01-15 15:12:39 +0200

    cli skeleton

--- agata_programm_folder/agata_skeleton.py
+++ agata_programm_folder/agata_skeleton.py
@@ -1,16 +1,13 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib as plt
-import scipy
-import mahotas
-import SimpleITK
-import sklearn
+import click
 
 
-def numpyarray(value):
-    value = np.array([value]) #numpy for cheking this library via virtual environment
-    print(value)
+@click.command()
+@click.option('--dcom', '-d', help='Path to dcom file')
+@click.option('--jpeg', '-j', help='Path to folder with jpeg')
+def dcom_to_jpeg(dcom, jpeg):
+    print('The location of dcom file is {} , and path to folder with jpeg is {}'.format(dcom, jpeg))
 
 
-numpyarray(14)
+if __name__ == '__main__':
+    dcom_to_jpeg()
+
