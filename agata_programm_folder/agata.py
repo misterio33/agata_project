ff298b70cdade9eeb2602a64f07a52884341da97
misterio33 <pasha.onoprienko@gmail.com>
2020-01-16 11:43:42 +0200

    agata.py with empty frames for functions

--- agata_programm_folder/agata.py
+++ agata_programm_folder/agata.py
@@ -1,21 +1,33 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib as plt
-import scipy
-import mahotas
-import SimpleITK
-import sklearn
 import click
 
 
-@click.command()
-@click.option('--dcom', '-d', help='Path to dcom file')
-@click.option('--jpeg', '-j', help='Path to folder with jpeg')
-def dcom_to_jpeg(dcom, jpeg):
-    print('The location of dcom file is {} , and path to folder with jpeg is {}'.format(dcom, jpeg))
+@click.group()
+def cli1():
+    pass
 
+
+@cli1.command()
+@click.option('--dicom', '-d', help='Path to dicom file')
+@click.option('--png', '-p', help='Path to folder with png')
+def dicom_to_png(dicom, png):
+    """Command for converting dicom files to png format"""
+    print('The location of dicom file is {} , and path to folder with png is {}'.format(dicom, png))
+
+
+@click.group()
+def cli2():
+    pass
+
+
+@cli2.command()
+@click.option('--dicom', '-d', help='Path to dicom file')
+@click.option('--jpeg', '-j', help='Path to folder with jpeg')
+def dicom_to_jpeg(dicom, jpeg):
+    """Command for converting dicom files to jpeg format"""
+    print('The location of dicom file is {} , and path to folder with jpeg is {}'.format(dicom, jpeg))
+
+
+cli = click.CommandCollection(sources=[cli1, cli2])
 
 if __name__ == '__main__':
-    dcom_to_jpeg()
-
+    cli()
