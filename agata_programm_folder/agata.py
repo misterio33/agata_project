3562be1d7f19aba3b77d65760d96da81e656c43d
misterio33 <pasha.onoprienko@gmail.com>
2020-01-20 09:56:41 +0200

    converter to jpg with correct names

--- agata_programm_folder/agata.py
+++ agata_programm_folder/agata.py
@@ -9,11 +9,11 @@ def cli1():
 
 
 @cli1.command()
-@click.option('--dicompath', help='Path to folder with dicom file, for example : /home/usr/DICOM_FOLDER')
+@click.option('--dicompath', help='Path to folder with .dcm files, for example : /home/usr/DICOM_FOLDER')
 @click.option('--outputpath', help='Path to folder of converted files, for example: /home/usr/OUTPUT_FOLDER, '
                                    'if this folder does not exist, a new one will be created')
 def convert(dicompath, outputpath):
-    # Specify the input dicom folder path
+    # Specify the input .dcm folder path
     # No matter does folder path has '/' at the end or not
     if dicompath[-1] == '/':
         folder_path = dicompath
@@ -35,17 +35,17 @@ def convert(dicompath, outputpath):
     for n, images in enumerate(images_path):
         # Deleting '.dcm' from file name
         if '.dcm' in images:
-            imagename = images.replace('.dcm', '')
+            image_name = images.replace('.dcm', '')
         else:
-            imagename = images
-        # Reading dicom image
+            image_name = images
+        # Reading .dcm image
         img = sitk.ReadImage(folder_path + images)
         # rescale intensity range from [-1000,1000] to [0,255]
         img = sitk.IntensityWindowing(img, -1000, 1000, 0, 255)
         # convert 16-bit pixels to 8-bit
         img = sitk.Cast(img, sitk.sitkUInt8)
         # Writing converted png image
-        sitk.WriteImage(img, output_folder_path + imagename + '.png')
+        sitk.WriteImage(img, output_folder_path + image_name + '.png')
 
 
 cli = click.CommandCollection(sources=[cli1])
