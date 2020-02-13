fee68c40cfefdfc37ff7e1500f261f318d06fafe
misterio33 <pasha.onoprienko@gmail.com>
2020-02-13 21:29:00 +0200

    creating png masks from json files in folder

--- app/agata.py
+++ app/agata.py
@@ -23,11 +23,13 @@ def cli2():
 
 
 @cli2.command()
-@click.option('--json_file', help='Name of json file, it must be at the same location as agata.py'
+@click.option('--json_path', help='Name of json file, it must be at the same location as agata.py'
                                   '(temporally, will be changed soon)')
-def jsonmask(json_file):
+@click.option('--output_path', help='Path to folder of converted files, for example: /home/usr/OUTPUT_FOLDER, '
+                                    'if this folder does not exist, a new one will be created')
+def jsonmask(json_path, output_path):
     json = MaskCreatorFromJSON()
-    json.mask_from_json(json_file)
+    json.mask_from_json(json_path, output_path)
 
 
 cli = click.CommandCollection(sources=[cli1, cli2])
